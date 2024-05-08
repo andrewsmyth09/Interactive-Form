ffe853a63bb7d3272f2844493ed294a19b3b80ef
andrewsmyth09 <asmyth@gmx.com>
2024-05-08 18:43:33 +0200

    Update script.js

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -8,18 +8,24 @@ const colorSelect = document.getElementById('color');
 const designSelect = document.getElementById('design');
 const activityRegister = document.getElementById('activities');
 const total_cost_field = document.getElementById('activities-cost');
+const payment_menu = document.getElementById('payment');
 
-// Focus on the name field
+// REUSABLE FUNCTIONS
+
+const hideBlock = block => block.style.display = 'none';
+const showBlock = block => block.style.display = 'block';
+
+// Focus on the name field.
 
 nameField.focus();
 
-// Hide the "other job" text field
+// Hide the "other job" text field.
 
-otherJobField.style.display = 'none';
+hideBlock(otherJobField);
 
 // Select additional input field for when user selects the "Other" option from the job role dropdown menu.
 
-jobSelector.addEventListener('change', (event) => event.target.value === 'other' ? otherJobField.style.display = 'block' : otherJobField.style.display = 'none');
+jobSelector.addEventListener('change', (event) => event.target.value === 'other' ? showBlock(otherJobField) : hideBlock(otherJobField));
 
 // Disable the color select element
 
@@ -37,7 +43,7 @@ designSelect.addEventListener('change', (event) => {
     });
 });
 
-// Reflect the total cost of all selected items in the 'Register for Activities' section
+// Reflect the total cost of all selected items in the 'Register for Activities' section.
 let total_cost = 0;
 
 activityRegister.addEventListener('change', (event) => {
@@ -46,4 +52,22 @@ activityRegister.addEventListener('change', (event) => {
     
     checkedStatus ? total_cost += activityPrice : total_cost -= activityPrice;
     total_cost_field.textContent = `Total: $${total_cost}`
-})
+});
+
+// Show the appropriate payment section for the user's chosen payment method.
+
+const creditCard = document.getElementById('credit-card');
+const paypal = document.getElementById('paypal');
+const bitcoin = document.getElementById('bitcoin');
+
+hideBlock(paypal);
+hideBlock(bitcoin);
+
+payment_menu.addEventListener('change', (event) => {
+    const userMethod = event.target.value;
+    const paymentOptions = [creditCard, paypal, bitcoin];
+
+    paymentOptions.forEach(option => option.id === userMethod ? showBlock(option) : hideBlock(option));
+});
+
+// Validate the user's input in the form.
